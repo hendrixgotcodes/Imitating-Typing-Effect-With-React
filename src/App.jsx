389a7f29d37767173071b07f2718e8e160f86e6c
Samuel Opoku Asare <asare11samuel@gmail.com>
2021-07-28 06:57:56 +0000

    Changed names of fields/states to reflect their actual function or use

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ const words = ["Hello there! ", "I ", "am ", "trying ", "to ", "imitate ", "typi
 
 function App() {
 
-  const [index, setIndex] = useState(0)
-  const [subIndex, setSubIndex] = useState(0)
-  const [reverse, setReverse] = useState(false)
-  const [blink, setBlink] = useState(true)
+  const [currentWord, setCurrentWord] = useState(0)
+  const [currentLetter, setCurrentLetter] = useState(0)
+  const [isReversing, setIsReversing] = useState(false)
+  const [showCaret, setShowCaret] = useState(true)
 
   useEffect(() => {
 
     const timeout2 = setTimeout(() => {
 
-      //Check for a 'truthy' value, if not make it falsy
-      setBlink((prev) => !prev);
+      //Check if caret is shown. If not shown, show. If shown, hide.
+      setShowCaret((prev) => !prev);
 
     }, 500);
 
@@ -24,31 +24,31 @@ function App() {
     return () => clearTimeout(timeout2);
 
 
-  }, [blink]);
+  }, [showCaret]);
 
   useEffect(() => {
 
     //If we've exhausted every word in the 'words' array, stop
-    if (index === words.length) return;
+    if (currentWord === words.length) return;
 
     //else if we've reach end of the current word && we are not on the last word && we are not reversing
-    if (subIndex === words[index].length + 1 && index !== words.length - 1 && !reverse){
+    if (currentLetter === words[currentWord].length + 1 && currentWord !== words.length - 1 && !isReversing){
 
       //we should reverse
-      setReverse(true);
+      setIsReversing(true);
       return;
 
 
     }
      
     //if we have reversed to the last letter of the current word, 
-    if (subIndex === 0 && reverse) {
+    if (currentLetter === 0 && isReversing) {
 
       //stop reversing (i mean, whats the point 🙄)
-      setReverse(false);
+      setIsReversing(false);
 
       //move on to the next word in the 'words' array
-      setIndex((prev) => prev + 1);
+      setCurrentWord((prev) => prev + 1);
       return;
 
 
@@ -57,19 +57,18 @@ function App() {
     //else
     const timeout = setTimeout(() => {
       //move on to the next letter of the current word (if reversing, move on to the previous letter)
-      setSubIndex((prev) => prev + (reverse ? -1 : 1));
+      setCurrentLetter((prev) => prev + (isReversing ? -1 : 1));
     }, 200);
      
     return () => clearTimeout(timeout);
 
-  }, [subIndex, index, reverse]);
-  //  ...
+  }, [currentLetter, currentWord, isReversing]);
   
 
 
   return (
     <div className="App">
-      <h1> {`${words[index].substring(0, subIndex)}${blink ? '|' : ''}`} </h1>
+      <h1> {`${words[currentWord].substring(0, currentLetter)}${showCaret ? '|' : ''}`} </h1>
     </div>
   );
 }
